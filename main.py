48d2dafdfdf76f6c84a2368352ab6db39c68aaf3
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-21 17:39:55 -0500

    Done

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
-import json
+import time
 from network.initialize_network import initialize_network
 from Config_files.config_load import load_all_configs
-from network.ue import UE
 from visualization.plot_network import plot_network
 
 def main():
@@ -16,14 +15,18 @@ def main():
     # Initialize gNodeBs, Cells, and UEs
     gNodeBs, cells, ues = initialize_network(num_ues_to_launch)
     
-    # Now visualize the network
+    # Initialize visualization
     plot_network(gNodeBs, ues, show_cells=True, background_image_path='images/worcester_map.jpg')
-    
+
     # Run the simulation indefinitely
     while True:
         for ue in ues:
             data_size, interval = ue.generate_traffic()
             print(f"UE ID: {ue.ID}, Service Type: {ue.ServiceType}, Data Size: {data_size} KB/MB, Interval: {interval} sec")
+        
+        # Update the visualization at a specified interval (e.g., every 10 seconds)
+        time.sleep(10)
+        plot_network(gNodeBs, ues, show_cells=True, background_image_path='images/worcester_map.jpg')
 
 if __name__ == "__main__":
     main()
