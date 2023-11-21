fc379862e9ace3a1df9658447b44e8c378f2b581
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-21 17:54:40 -0500

    D

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import os
 import time
+from multiprocessing import Process
 from network.initialize_network import initialize_network
 from Config_files.config_load import load_all_configs
 from visualization.plot_network import plot_network
+
+def visualize_network(gNodeBs, ues):
+    while True:
+        plot_network(gNodeBs, ues, show_cells=True, background_image_path='images/worcester_map.jpg')
+        time.sleep(10)  # Update interval for the visualization
+
+def log_traffic(ues):
+    while True:
+        for ue in ues:
+            data_size, interval = ue.generate_traffic()
+            print(f"UE ID: {ue.ID}, Service Type: {ue.ServiceType}, Data Size: {data_size} KB/MB, Interval: {interval} sec")
+        time.sleep(1)  # Logging interval
 
 def main():
     # Load configurations
@@ -15,18 +28,17 @@ def main():
     # Initialize gNodeBs, Cells, and UEs
     gNodeBs, cells, ues = initialize_network(num_ues_to_launch)
     
-    # Initialize visualization
-    plot_network(gNodeBs, ues, show_cells=True, background_image_path='images/worcester_map.jpg')
+    # Create a separate process for visualization
+    visualization_process = Process(target=visualize_network, args=(gNodeBs, ues))
+    visualization_process.start()
 
-    # Run the simulation indefinitely
-    while True:
-        for ue in ues:
-            data_size, interval = ue.generate_traffic()
-            print(f"UE ID: {ue.ID}, Service Type: {ue.ServiceType}, Data Size: {data_size} KB/MB, Interval: {interval} sec")
-        
-        # Update the visualization at a specified interval (e.g., every 10 seconds)
-        time.sleep(10)
-        plot_network(gNodeBs, ues, show_cells=True, background_image_path='images/worcester_map.jpg')
+    # Create a separate process for logging
+    logging_process = Process(target=log_traffic, args=(ues,))
+    logging_process.start()
+
+    # Wait for the processes to complete (they won't in this case since they run indefinitely)
+    visualization_process.join()
+    logging_process.join()
 
 if __name__ == "__main__":
     main()
