3761a1fa10445854d76b4f8e4c9dc910d4a5a7c6
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-25 20:46:51 -0500

    Fix bug

--- main.py
+++ main.py
@@ -16,11 +16,24 @@ def visualize_network(gNodeBs, ues):
         plot_network(gNodeBs, ues, show_cells=True, background_image_path='images/worcester_map.jpg')
         time.sleep(10)  # Update interval for the visualization
 
-def log_traffic(ues):
+def log_traffic(ues, db_manager):
     while True:
         for ue in ues:
             data_size, interval = ue.generate_traffic()
             print(f"UE ID: {ue.ID}, IMEI: {ue.IMEI}, Service Type: {ue.ServiceType}, Data Size: {data_size}, Interval: {interval} sec")
+            
+            # Prepare data for logging to the database
+            data = {
+                'ue_id': ue.ID,
+                'imei': ue.IMEI,
+                'service_type': ue.ServiceType,
+                'data_size': data_size,
+                # Add other relevant data fields here
+            }
+
+            # Log the data to the database
+            db_manager.insert_ue_data(data)
+
             time.sleep(1)  # Logging interval
 
 def main():
@@ -33,13 +46,16 @@ def main():
 
     # Initialize gNodeBs, Cells, and UEs
     gNodeBs, cells, ues = initialize_network(num_ues_to_launch, gNodeBs_config, cells_config, ue_config)
-    
+
+    # Initialize DatabaseManager
+    db_manager = DatabaseManager()
+
     # Create a separate process for visualization
     visualization_process = Process(target=visualize_network, args=(gNodeBs, ues))
     visualization_process.start()
 
     # Create a separate process for logging
-    logging_process = Process(target=log_traffic, args=(ues,))
+    logging_process = Process(target=log_traffic, args=(ues, db_manager))
     logging_process.start()
 
     # Wait for the processes to complete (they won't in this case since they run indefinitely)
