49478e179bd437be9ed020e563290900291a065b
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-29 16:38:21 -0500

    fyi

--- dashboard/app_instance.py
+++ dashboard/app_instance.py
@@ -4,5 +4,5 @@ import dash_bootstrap_components as dbc
 from flask import Flask
 
 server = Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
