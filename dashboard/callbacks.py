907a4101676b4d64d3b9691039bbce1c2a2a6dbd
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-29 16:25:02 -0500

    FYI

--- dashboard/callbacks.py
+++ dashboard/callbacks.py
@@ -1,10 +1,12 @@
 # callbacks.py
+import html
 from dash import Output, Input, State, dcc
 from app_instance import app  # Import the Dash app instance
 from layouts import ue_level_report_layout, cell_level_report_layout, gnodb_level_report_layout, network_level_report_layout
 from layouts import main_layout
 def check_credentials(username, password):
     return username == "admin" and password == "admin"
+
 
 def register_callbacks(app):
     @app.callback(
@@ -17,17 +19,20 @@ def register_callbacks(app):
     def toggle_modal(n_clicks, username, password):
         if n_clicks > 0:
             if check_credentials(username, password):
-                return False, main_layout()  # Close modal and display main layout
+            # On successful login, close the modal and display the main layout
+                return False, main_layout()
             else:
-                return True, None  # Keep modal open and main layout empty
-        return True, None  # Default:
+            # Keep the modal open on failed login
+                return True, None
+    # Default state when the page loads
+    return True, None
 
-    @app.callback(
+@app.callback(
         Output("tab-content", "children"),
         [Input("tabs", "active_tab")],
         prevent_initial_call=True
     )
-    def switch_tab(active_tab):
+def switch_tab(active_tab):
         if active_tab == "tab-ue":
             return ue_level_report_layout()
         elif active_tab == "tab-cell":
@@ -36,7 +41,11 @@ def register_callbacks(app):
             return gnodb_level_report_layout()
         elif active_tab == "tab-network":
             return network_level_report_layout()
-        return html.Div()  # Default return if no tab is selected
+        return html.Div()  # Default return if no tab is selected</selectedCode>
+
+
+
+
 
 
 
