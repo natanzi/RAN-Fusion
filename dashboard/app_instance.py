446223ed3dc37a5fafb271d7b3fd7f8f3db25090
Milad Natanzi <miladnatanzi@gmail.com>
2023-11-29 16:36:15 -0500

    FYI

--- dashboard/app_instance.py
+++ dashboard/app_instance.py
@@ -4,5 +4,5 @@ import dash_bootstrap_components as dbc
 from flask import Flask
 
 server = Flask(__name__)
-app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
--- dashboard/layouts.py
+++ dashboard/layouts.py
@@ -24,19 +24,27 @@ def create_login_modal():
     )
 
 def main_layout():
-    return html.Div(id='main-layout-container', children=[
-        dbc.Tabs(id="tabs", children=[
-            dbc.Tab(label="UE Level Report", tab_id="tab-ue"),
-            dbc.Tab(label="Cell Level Report", tab_id="tab-cell"),
-            dbc.Tab(label="gNodeB Level Report", tab_id="tab-gnodb"),
-            dbc.Tab(label="Network Level Report", tab_id="tab-network"),
-         ], active_tab="tab-ue"),
-            html.Div(id="tab-content", children=[
-            html.Iframe(
-                id='map',
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.336261614217!2d-71.80329368420207!3d42.26259337919353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e4065daf44c81d%3A0x450d8f1f1330361!2sWorcester%2C%20MA%2C%20USA!5e0!3m2!1sen!2s!4v1631024739346",
-                style={"height": "100vh", "width": "100%"}
-            ),
+    return dbc.Container(fluid=True, children=[
+        dbc.Row([
+            dbc.Col([
+                # Sidebar content with tabs
+                dbc.Tabs(id="tabs", children=[
+                    dbc.Tab(label="UE Level Report", tab_id="tab-ue"),
+                    dbc.Tab(label="Cell Level Report", tab_id="tab-cell"),
+                    dbc.Tab(label="gNodeB Level Report", tab_id="tab-gnodb"),
+                    dbc.Tab(label="Network Level Report", tab_id="tab-network"),
+                ], active_tab="tab-ue"),
+                html.Div(id="tab-content"),  # Content of the selected tab
+            ], width=3),  # Width of the sidebar (3 out of 12 columns)
+
+            dbc.Col([
+                # Google Maps iframe
+                html.Iframe(
+                    id='map',
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2948.336261614217!2d-71.80329368420207!3d42.26259337919353!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e4065daf44c81d%3A0x450d8f1f1330361!2sWorcester%2C%20MA%2C%20USA!5e0!3m2!1sen!2s!4v1631024739346",
+                    style={"height": "100vh", "width": "100%"}
+                ),
+            ], width=9),  # Width of the map area (9 out of 12 columns)
         ]),
     ])
 # Placeholder functions for each report's content
