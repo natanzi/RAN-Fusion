a924a3c27b912f3ca9cbc0ea680a8a8da08b856b
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-03 01:32:42 -0500

    FYI

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,16 +3,22 @@ from dash import html
 from app_instance import app
 from layouts import create_login_modal, create_sidebar, create_main_content
 from callbacks import register_callbacks
-from layouts import create_login_modal, create_main_content, create_sidebar
+
+# Register the callbacks with the app
 register_callbacks(app)
 
-# Ensure all these functions return valid Dash components
-app.layout = html.Div([
-    create_login_modal(),
-    create_sidebar(),
-    create_main_content(),
-    html.Div(id='main-content', style={'display': 'none'})
-], className='body-background')
+# Define the app layout in a function to make it clear and reusable
+def create_app_layout():
+    return html.Div([
+        create_login_modal(),
+        create_sidebar(),
+        create_main_content(),
+        html.Div(id='main-content', style={'display': 'none'})
+    ], className='body-background')
+
+# Set the layout of the app
+app.layout = create_app_layout()
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    # Turn off debug mode in production!
+    app.run_server(debug=False)
