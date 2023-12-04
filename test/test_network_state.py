e502f56251f0c109ec045631dd9a180d5b6fcb4b
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-04 15:28:39 -0500

    fyi

--- test/test_network_state.py
+++ test/test_network_state.py
@@ -1,27 +1,33 @@
 #to insure that network state class is working
 #test_network_state.py located in test folder
+# test_network_state.py located in test folder
 from network.network_state import NetworkState
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# Assuming gNodeBs, cells, and ues have been initialized and populated elsewhere in your code
+from main import load_all_configs, initialize_network
 
-def find_ue_info(ue_id):
+def test_network_state_functionality():
+    # Load configurations as done in main.py
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    gNodeBs_config, cells_config, ue_config = load_all_configs(base_dir)
+
+    # Initialize network components as done in main.py
+    num_ues_to_launch = 10  # Or any other number suitable for testing
+    gNodeBs, cells, ues = initialize_network(num_ues_to_launch, gNodeBs_config, cells_config, ue_config)
+
     # Instantiate the NetworkState class
     network_state = NetworkState()
 
     # Update the network state with the initialized components
     network_state.update_state(gNodeBs, cells, ues)
 
-    # Retrieve the information for UE with ID 4
-    ue_info = network_state.get_ue_info(ue_id)
+    # Test the get_ue_info method for a known UE
+    ue_id_to_test = 4  # Assuming UE with ID 4 exists
+    ue_info = network_state.get_ue_info(ue_id_to_test)
 
-    return ue_info
+    # Assert that ue_info is not None and contains expected data
+    assert ue_info is not None, "UE information should not be None"
+    # Add more assertions based on the expected state of UE with ID 4
 
-# Call the function with UE ID 4
-ue_info = find_ue_info(4)
+    print(f"Test passed for UE {ue_id_to_test}: {ue_info}")
 
-if ue_info:
-    print(f"Information for UE 4: {ue_info}")
-else:
-    print("UE 4 information could not be found.")
+# Run the test
+test_network_state_functionality()
