83935a9c378bed1da16fd36c5c44b417944f86a9
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-04 14:14:54 -0500

    FYI

--- test/test_network_state.py
+++ test/test_network_state.py
@@ -1,11 +1,26 @@
 #to insure that network state class is working
 #test_network_state.py located in test folder
-from network.network_state import NetworkState  
-# To get the UE information including its cell and gNodeB
-ue_info = network_state.get_ue_info('UE40')
+from network.network_state import NetworkState
 
-# To get the cell information including its gNodeB and allocated cells
-cell_info = network_state.get_cell_info('AX340T')
+# Assuming gNodeBs, cells, and ues have been initialized and populated elsewhere in your code
 
-# To get the last update of a specific gNodeB and its allocated cells
-gNodeB_info = network_state.get_gNodeB_last_update('T240')
+def find_ue_info(ue_id):
+    # Instantiate the NetworkState class
+    network_state = NetworkState()
+
+    # Update the network state with the initialized components
+    # This should be done after the network has been initialized and the dictionaries have been populated
+    network_state.update_state(gNodeBs, cells, ues)
+
+    # Retrieve the information for UE with ID 4
+    ue_info = network_state.get_ue_info(ue_id)
+
+    return ue_info
+
+# Call the function with UE ID 4
+ue_info = find_ue_info(4)
+
+if ue_info:
+    print(f"Information for UE 4: {ue_info}")
+else:
+    print("UE 4 information could not be found.")
