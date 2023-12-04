b8d757891aec4b2a57c3ca50fd0672727f3814bb
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-04 13:07:04 -0500

    FYI

--- network/init_gNodeB.py
+++ network/init_gNodeB.py
@@ -10,5 +10,6 @@ def load_json_config(file_path):
 
 def initialize_gNodeBs(gNodeBs_config):
     # Initialize gNodeBs using the provided configuration
-    gNodeBs = [gNodeB(**gNodeB_data) for gNodeB_data in gNodeBs_config['gNodeBs']]
+    # Change from list to dictionary with gNodeB_id as keys
+    gNodeBs = {gNodeB_data['gnodeb_id']: gNodeB(**gNodeB_data) for gNodeB_data in gNodeBs_config['gNodeBs']}
     return gNodeBs
