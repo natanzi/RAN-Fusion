c98bcdbe097a9762c5308ec457d24cd93abc4ff3
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-04 12:35:19 -0500

    Update

--- network/cell.py
+++ network/cell.py
@@ -1,6 +1,7 @@
 #Defines the Cell class, which is part of a gNodeB.// this is located inside network directory
 import logging
-
+from .network_state import NetworkState  # Import the NetworkState class
+import datetime
 class Cell:
     def __init__(self, cell_id, gnodeb_id, frequencyBand, duplexMode, tx_power, bandwidth, ssb_periodicity, ssb_offset, max_connect_ues, channel_model, trackingArea=None):
         self.ID = cell_id
@@ -63,13 +64,16 @@ class Cell:
             }
         return len(self.ConnectedUEs)
 
-    def add_ue(self, ue):
+    def add_ue(self, ue, network_state):
         if len(self.ConnectedUEs) < self.MaxConnectedUEs:
             self.ConnectedUEs.append(ue)
             print(f"UE '{ue.ID}' has been attached to Cell '{self.ID}'.")
+            self.update_ue_count()
+        # Update the network state here
+            network_state.update_state(network_state.gNodeBs, network_state.cells, network_state.ues)
         else:
             raise Exception("Maximum number of connected UEs reached for this cell.")
-            self.update_ue_count()
+
     # You may also want to override the method that removes a UE to include the update_ue_count call
     def remove_ue(self, ue):
         # ... (code to remove UE)
--- network/initialize_network.py
+++ network/initialize_network.py
@@ -3,6 +3,7 @@
 from .init_gNodeB import initialize_gNodeBs  # Import the new initialization function
 from .init_cell import initialize_cells  # Import the new Cell initialization function
 from .init_ue import initialize_ues  # Import the new UE initialization function
+from .network_state import NetworkState  # Import the NetworkState class
 
 def initialize_network(num_ues_to_launch, gNodeBs_config, cells_config, ue_config):
     # Initialize gNodeBs with the provided configuration
@@ -14,5 +15,9 @@ def initialize_network(num_ues_to_launch, gNodeBs_config, cells_config, ue_confi
 
     # After initializing gNodeBs and cells, initialize UEs with the provided configuration
     ues = initialize_ues(num_ues_to_launch, gNodeBs, ue_config)
+    # Create an instance of NetworkState
+    network_state = NetworkState()
 
+    # Update the network state with the initialized elements
+    network_state.update_state(gNodeBs, cells, ues)
     return gNodeBs, cells, ues
