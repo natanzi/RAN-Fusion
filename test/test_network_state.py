ad0d2a7dc4cc4fd34619379010f802d7031522e4
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-04 16:05:18 -0500

    FYI

--- test/test_network_state.py
+++ test/test_network_state.py
@@ -9,8 +9,11 @@ network_state = NetworkState()
 # You would update the network state with these dictionaries
 # network_state.update_state(gNodeBs, cells, ues)
 
-# Now, you can get the UE information for UE with ID 'UE10'
-ue_info = network_state.get_ue_info('UE10')
+# Prompt the user to enter the UE ID
+ue_id_input = input("Please enter the UE ID like 10: ")
+
+# Now, you can get the UE information for the entered UE ID
+ue_info = network_state.get_ue_info(ue_id_input)
 
 if ue_info:
     print(f"UE ID: {ue_info['UE_ID']}")
@@ -18,4 +21,4 @@ if ue_info:
     print(f"gNodeB ID: {ue_info['gNodeB_ID']}")
     print(f"Neighbor Cells: {ue_info['Neighbors']}")
 else:
-    print("UE with ID 'UE10' not found.")
+    print(f"UE with ID '{ue_id_input}' not found.")
