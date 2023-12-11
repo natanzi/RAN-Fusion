025ccb4dbd2153b5cdca4f5a63b7a8645965f012
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-11 14:30:00 -0500

    FYI

--- control_app.py
+++ control_app.py
@@ -1,6 +1,10 @@
 # control_app.py in root directory
 from flask import Flask, request
 from traffic.traffic_generator import TrafficController
+from log.logger_config import setup_logger
+
+# Set up a logger for the traffic updates
+traffic_update_logger = setup_logger('traffic_update', 'logs/traffic_update.log')
 
 app = Flask(__name__)
 traffic_controller = TrafficController()
@@ -18,6 +22,7 @@ def update_voice_traffic():
     packet_loss_rate = data.get('packet_loss_rate')
     traffic_controller.update_voice_traffic(bitrate_range)
     traffic_controller.update_voice_traffic_parameters(jitter, delay, packet_loss_rate)
+    traffic_update_logger.info(f'Voice traffic updated: bitrate_range={bitrate_range}, jitter={jitter}, delay={delay}, packet_loss_rate={packet_loss_rate}')
     return {'message': 'Voice traffic parameters updated successfully'}, 200
 
 @app.route('/update_video_traffic', methods=['POST'])
@@ -30,6 +35,7 @@ def update_video_traffic():
     packet_loss_rate = data.get('packet_loss_rate')
     traffic_controller.update_video_traffic(num_streams_range, stream_bitrate_range)
     traffic_controller.update_video_traffic_parameters(jitter, delay, packet_loss_rate)
+    traffic_update_logger.info(f'Video traffic updated: num_streams_range={num_streams_range}, stream_bitrate_range={stream_bitrate_range}, jitter={jitter}, delay={delay}, packet_loss_rate={packet_loss_rate}')
     return {'message': 'Video traffic parameters updated successfully'}, 200
 
 @app.route('/update_gaming_traffic', methods=['POST'])
@@ -41,6 +47,7 @@ def update_gaming_traffic():
     packet_loss_rate = data.get('packet_loss_rate')
     traffic_controller.update_gaming_traffic(bitrate_range)
     traffic_controller.update_gaming_traffic_parameters(jitter, delay, packet_loss_rate)
+    traffic_update_logger.info(f'Gaming traffic updated: bitrate_range={bitrate_range}, jitter={jitter}, delay={delay}, packet_loss_rate={packet_loss_rate}')
     return {'message': 'Gaming traffic parameters updated successfully'}, 200
 
 @app.route('/update_iot_traffic', methods=['POST'])
@@ -53,6 +60,7 @@ def update_iot_traffic():
     packet_loss_rate = data.get('packet_loss_rate')
     traffic_controller.update_iot_traffic(packet_size_range, interval_range)
     traffic_controller.update_iot_traffic_parameters(jitter, delay, packet_loss_rate)
+    traffic_update_logger.info(f'IoT traffic updated: packet_size_range={packet_size_range}, interval_range={interval_range}, jitter={jitter}, delay={delay}, packet_loss_rate={packet_loss_rate}')
     return {'message': 'IoT traffic parameters updated successfully'}, 200
 
 @app.route('/update_data_traffic', methods=['POST'])
@@ -65,6 +73,7 @@ def update_data_traffic():
     packet_loss_rate = data.get('packet_loss_rate')
     traffic_controller.update_data_traffic(bitrate_range, interval_range)
     traffic_controller.update_data_traffic_parameters(jitter, delay, packet_loss_rate)
+    traffic_update_logger.info(f'Data traffic updated: bitrate_range={bitrate_range}, interval_range={interval_range}, jitter={jitter}, delay={delay}, packet_loss_rate={packet_loss_rate}')
     return {'message': 'Data traffic parameters updated successfully'}, 200
 
 if __name__ == '__main__':
