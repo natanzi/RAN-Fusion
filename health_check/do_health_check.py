3420bec4150ae3e0eea0260931e544597738aedf
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-12 11:29:07 -0500

    FYI

--- health_check/do_health_check.py
+++ health_check/do_health_check.py
@@ -1,13 +1,23 @@
 #do_health_check.py located in health_check directory
+import sys
 from .server_status import check_server_status
 from .database_status import check_database_status
 from .system_resources import check_system_resources
 
 def perform_health_check(network_state):
     print("Performing health check...")
-    print("Server Status:", check_server_status())
-    print("Database Status:", check_database_status(network_state))  # Pass network_state here
-    print("System Resources:", check_system_resources())
+    server_status = check_server_status()
+    database_status = check_database_status(network_state)  # Pass network_state here
+    system_resources = check_system_resources()
+
+    print("Server Status:", server_status)
+    print("Database Status:", database_status)
+    print("System Resources:", system_resources)
+
+    # Check if any of the components report a status other than 'UP'
+    if server_status['status'] != 'UP' or database_status['status'] != 'UP':
+        print("Health check failed. Exiting...")
+        sys.exit(1)  # Exit with a non-zero status to indicate an error
 
 if __name__ == "__main__":
     perform_health_check()
