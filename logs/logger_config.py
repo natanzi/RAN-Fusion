3d5b5e87a1c4662d5d4a378e2284f3c8a5927b36
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-15 17:24:56 -0500

    FYI

--- logs/logger_config.py
+++ logs/logger_config.py
@@ -16,7 +16,6 @@ class JsonFormatter(logging.Formatter):
         }
         if record.exc_info:
             log_record['exception'] = self.formatException(record.exc_info)
-        return json.dumps(log_record)
 
 def setup_logger(name, log_file, level=logging.INFO, max_log_size=10 * 1024 * 1024, backup_count=5):
     """Function to set up a logger with asynchronous and buffered logging."""
