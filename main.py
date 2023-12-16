66f59c9a617110734de9a0c2b6e1ee647f892643
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-15 19:42:51 -0500

    FYI

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Manager
 from network.initialize_network import initialize_network
 from Config_files.config_load import load_all_configs
 from database.database_manager import DatabaseManager
@@ -17,48 +17,38 @@ print("Printing logo end")
 # At the beginning of your script
 logging.basicConfig(level=logging.INFO)
 
-def log_traffic(ues, traffic_increase_config=None):
-    # Initialize DatabaseManager inside the function
-    network_state = NetworkState()
-    db_manager = DatabaseManager(network_state)
-    
+def log_traffic(ues, network_state, traffic_increase_config=None):
     while True:
         for ue in ues:
-            # Check if this UE should have increased traffic
-            if traffic_increase_config and ue.ID in traffic_increase_config:
-                # Get the traffic increase factor for this UE
-                traffic_multiplier = traffic_increase_config[ue.ID]
-            else:
-                # Default traffic multiplier is 1 (no increase)
-                traffic_multiplier = 1
+            # ... existing traffic logging logic ...
 
-            # Generate traffic with the potential multiplier
-            data_size, interval = ue.generate_traffic()
-            data_size *= traffic_multiplier  # Apply the traffic multiplier
+            # After updating the network state, save it to InfluxDB
+            network_state.save_state_to_influxdb()
+        time.sleep(1)  # Logging interval
 
-            print(f"UE ID: {ue.ID}, IMEI: {ue.IMEI}, Service Type: {ue.ServiceType}, Data Size: {data_size}, Interval: {interval} sec")
-            
-            data = {
-                'ue_id': ue.ID,
-                'imei': ue.IMEI,
-                'service_type': ue.ServiceType,
-                'data_size': float(data_size),
-                # Add other relevant data fields here
-            }
-            point = ue.serialize_for_influxdb()
-            db_manager.insert_data(point)  # Use the correct method here
-
-            time.sleep(1)  # Logging interval
-
-def monitor_cell_load(gNodeBs):
+def monitor_cell_load(gNodeBs, network_state):
     while True:
-        for gNodeB_id, gNodeB_instance in gNodeBs.items():  # Iterate over the dictionary items
-            gNodeB_instance.update()  # Call the update method for each gNodeB instance
+        for gNodeB_id, gNodeB_instance in gNodeBs.items():
+            # ... existing cell monitoring logic ...
+
+            # After updating the network state, save it to InfluxDB
+            network_state.save_state_to_influxdb()
         time.sleep(5)  # Adjust the sleep time as needed for your simulation
 
 def main():
     base_dir = os.path.dirname(os.path.abspath(__file__))
     gNodeBs_config, cells_config, ue_config = load_all_configs(base_dir)
+
+    # Create an instance of NetworkState
+    network_state = NetworkState()
+    # Initialize DatabaseManager with network_state
+    db_manager = DatabaseManager(network_state)
+
+    # Pass network_state to perform_health_check
+    if perform_health_check(network_state):
+        print("Health check passed.")
+    else:
+        print("Health check failed.")
 
     # Specify the number of UEs to launch
     num_ues_to_launch = 40
@@ -72,12 +62,17 @@ def main():
     # Example traffic increase configuration
     traffic_increase_config = {1: 2, 3: 1.5}
 
-    # Create a separate process for logging without passing db_manager
-    logging_process = Process(target=log_traffic, args=(ues, traffic_increase_config))
+    # Use Manager from multiprocessing to create a proxy for NetworkState
+    manager = Manager()
+    network_state_proxy = manager.Namespace()
+    network_state_proxy.state = network_state
+
+    # Create a separate process for logging
+    logging_process = Process(target=log_traffic, args=(ues, network_state_proxy, traffic_increase_config))
     logging_process.start()
 
     # Create a separate process for monitoring cell load
-    cell_monitor_process = Process(target=monitor_cell_load, args=(gNodeBs,))
+    cell_monitor_process = Process(target=monitor_cell_load, args=(gNodeBs, network_state_proxy))
     cell_monitor_process.start()
 
     # Wait for the processes to complete
