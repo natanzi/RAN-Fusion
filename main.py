5c03085fe6a095a25db4dae906710ac646a25b12
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-17 12:58:45 -0500

    fyi

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import logging
 from logo import create_logo
 from health_check.do_health_check import perform_health_check
 from network.network_state import NetworkState
+from network.handover_utils import handle_load_balancing  # Make sure to import this
 
 def network_state_manager(network_state, command_queue):
     while True:
@@ -30,13 +31,16 @@ def monitor_cell_load(gNodeBs, command_queue):
             # ... existing cell monitoring logic ...
             command_queue.put('save')
         time.sleep(5)  # Adjust the sleep time as needed for your simulation
-######NEW ADD####
-def detect_and_handle_congestion(network_state):
-    for cell_id, cell in network_state.cells.items():
-        cell_load = network_state.get_cell_load(cell)
-        if cell_load > 0.8:  # Assuming 0.8 is the congestion threshold
-            # Call the load balancing function from handover_utils.py
-            handle_load_balancing(cell.gNodeB_ID, network_state)
+
+def detect_and_handle_congestion(network_state, command_queue):
+    while True:
+        for cell_id, cell in network_state.cells.items():
+            cell_load = network_state.get_cell_load(cell)
+            if cell_load > 0.8:  # Assuming 0.8 is the congestion threshold
+                # Call the load balancing function from handover_utils.py
+                handle_load_balancing(cell.gNodeB_ID, network_state)
+                command_queue.put('save')  # Save state after handling congestion
+        time.sleep(10)  # Check for congestion at regular intervals
 
 def main():
     logo_text = create_logo()
@@ -44,55 +48,49 @@ def main():
     print(logo_text)
     print("Printing logo end")
 
-    # At the beginning of your script
     logging.basicConfig(level=logging.INFO)
 
     base_dir = os.path.dirname(os.path.abspath(__file__))
     gNodeBs_config, cells_config, ue_config = load_all_configs(base_dir)
 
-    # Create an instance of NetworkState
     network_state = NetworkState()
 
-    # Initialize DatabaseManager with network_state
     db_manager = DatabaseManager(network_state)
 
-    # Pass network_state to perform_health_check
     if perform_health_check(network_state):
         print("Health check passed.")
     else:
         print("Health check failed.")
+        return  # Exit if health check fails
 
-    # Specify the number of UEs to launch
     num_ues_to_launch = 40
 
-    # Initialize gNodeBs, Cells, and UEs
     gNodeBs, cells, ues = initialize_network(num_ues_to_launch, gNodeBs_config, cells_config, ue_config, db_manager)
 
     print(f"Number of UEs returned: {len(ues)}")
     time.sleep(2)
 
-    # Example traffic increase configuration
     traffic_increase_config = {1: 2, 3: 1.5}
 
-    # Create a Queue for inter-process communication
     command_queue = Queue()
 
-    # Create and start the NetworkState manager process
     ns_manager_process = Process(target=network_state_manager, args=(network_state, command_queue))
     ns_manager_process.start()
-    # Create a separate process for logging
+
     logging_process = Process(target=log_traffic, args=(ues, command_queue, traffic_increase_config))
     logging_process.start()
 
-    # Create a separate process for monitoring cell load
     cell_monitor_process = Process(target=monitor_cell_load, args=(gNodeBs, command_queue))
     cell_monitor_process.start()
 
-    # Wait for the processes to complete
+    congestion_process = Process(target=detect_and_handle_congestion, args=(network_state, command_queue))  # Start congestion monitoring
+    congestion_process.start()
+
+    # Wait for the processes to complete (if they ever complete)
     logging_process.join()
     cell_monitor_process.join()
+    congestion_process.join()  # Ensure the congestion process is joined
 
-    # Tell the NetworkState manager process to exit
     command_queue.put('exit')
     ns_manager_process.join()
 
