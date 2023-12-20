5f7e20c466684a44ceacbf218da507f45c609be9
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-20 18:51:50 -0500

    FYI

--- logs/logger_config.py
+++ logs/logger_config.py
@@ -7,16 +7,22 @@ from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from concurrent.futures import ThreadPoolExecutor
 from database.time_utils import get_current_time_ntp
+import uuid
+
+# Generate a unique test run ID
+TEST_RUN_ID = str(uuid.uuid4())
 
 class JsonFormatter(logging.Formatter):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, test_id=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.test_id = test_id
 
     def format(self, record):
         log_record = {
             "level": record.levelname,
             "timestamp": get_current_time_ntp(),
-            "message": record.getMessage()
+            "message": record.getMessage(),
+            "test_id": self.test_id  # Include the test identifier in the log record
         }
         if record.exc_info:
             log_record['exception'] = self.formatException(record.exc_info)
@@ -24,7 +30,7 @@ class JsonFormatter(logging.Formatter):
         return json.dumps(log_record)
 
 
-def setup_logger(name, log_file, level=logging.INFO, max_log_size=10 * 1024 * 1024, backup_count=5):
+def setup_logger(name, log_file, test_id, level=logging.INFO, max_log_size=10 * 1024 * 1024, backup_count=5):
     """Function to set up a logger with asynchronous and buffered logging."""
     logger = logging.getLogger(name)
     logger.setLevel(level)
@@ -37,7 +43,7 @@ def setup_logger(name, log_file, level=logging.INFO, max_log_size=10 * 1024 * 10
     # Create a rotating file handler with asynchronous logging
     file_handler = RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
     file_handler.setLevel(level)
-    formatter = JsonFormatter()
+    formatter = JsonFormatter(test_id=test_id)
 
     file_handler.setFormatter(formatter)
 
@@ -59,14 +65,14 @@ def compress_log_file(log_file):
         os.remove(log_file_backup)
 
 # Initialize loggers for all components
-ue_logger = setup_logger('ue_logger', 'logs/ue_logger.log')
-cell_logger = setup_logger('cell_logger', 'logs/cell_logger.log')
-gnodeb_logger = setup_logger('gnodeb_logger', 'logs/gnodeb_logger.log')
-cell_load_logger = setup_logger('cell_load_logger', 'logs/cell_load.log')
-traffic_update = setup_logger('traffic_update', 'logs/traffic_update.log')
-database_logger = setup_logger('database_logger', 'logs/database_logger.log')
-system_resource_logger = setup_logger('system_resource_logger', 'logs/system_resource.log')
-health_check_logger = setup_logger('health_check_logger', 'logs/health_check_logger.log')
+ue_logger = setup_logger('ue_logger', 'logs/ue_logger.log', TEST_RUN_ID)
+cell_load_logger = setup_logger('cell_load_logger', 'logs/cell_load.log', TEST_RUN_ID)
+gnodeb_logger = setup_logger('gnodeb_logger', 'logs/gnodeb_logger.log', TEST_RUN_ID)
+cell_load_logger = setup_logger('cell_load_logger', 'logs/cell_load.log'), TEST_RUN_ID
+traffic_update = setup_logger('traffic_update', 'logs/traffic_update.log', TEST_RUN_ID)
+database_logger = setup_logger('database_logger', 'logs/database_logger.log', TEST_RUN_ID)
+system_resource_logger = setup_logger('system_resource_logger', 'logs/system_resource.log', TEST_RUN_ID)
+health_check_logger = setup_logger('health_check_logger', 'logs/health_check_logger.log', TEST_RUN_ID)
 
 # Dictionary to hold loggers and their respective log files for compression
 log_files = {
