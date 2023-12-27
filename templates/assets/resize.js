248c1e99d1c795dd6eb46cfbad71512637c80ccc
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-27 15:49:59 -0500

    FYI

--- templates/assets/resize.js
+++ templates/assets/resize.js
@@ -1,8 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Existing code for resizing...
+    var isResizing = false;
+
+    var mapContainer = document.getElementById('map-container');
+    var tabsContainer = document.getElementById('tabs-container');
+    var divider = document.getElementById('divider');
+
+    divider.addEventListener('mousedown', function(e) {
+        isResizing = true;
+    });
+
+    document.addEventListener('mousemove', function(e) {
+        if (!isResizing) {
+            return;
+        }
+
+        var containerOffsetTop = mapContainer.offsetTop;
+        var newHeight = e.clientY - containerOffsetTop;
+
+        mapContainer.style.height = newHeight + 'px';
+        tabsContainer.style.height = 'calc(100vh - ' + newHeight + 'px - 30px)';
+    });
+
+    document.addEventListener('mouseup', function(e) {
+        isResizing = false;
+    });
 
     // Add event listener for form submission
-    var form = document.getElementById('traffic-form'); // Replace with your actual form ID
+    var form = document.getElementById('traffic-form'); // Ensure this ID matches your form
     form.addEventListener('submit', function(e) {
         e.preventDefault();
 
@@ -24,29 +49,25 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             // Handle validation errors
             console.error('Validation failed');
-            // Display an error message to the user
+            // Ideally, display an error message to the user in the UI
         }
     });
 });
 
 function validatePayload(data) {
     // Validation logic here...
-    return isValidType(data.bitrate_range, 'object') && // Arrays are 'object' type in JS
+    return isValidType(data.bitrate_range, 'object') &&
            Array.isArray(data.bitrate_range) &&
            data.bitrate_range.length === 2 &&
            isValidType(data.bitrate_range[0], 'number') &&
            isValidType(data.bitrate_range[1], 'number') &&
-           isValidType(data.jitter, ['number']) &&
-           isValidType(data.delay, ['number']) &&
-           isValidType(data.packet_loss_rate, ['number']);
+           isValidType(data.jitter, 'number') &&
+           isValidType(data.delay, 'number') &&
+           isValidType(data.packet_loss_rate, 'number');
 }
 
 function isValidType(value, expectedType) {
-    if (Array.isArray(expectedType)) {
-        return expectedType.includes(typeof value);
-    } else {
-        return typeof value === expectedType;
-    }
+    return typeof value === expectedType;
 }
 
 function sendPostRequest(url, data) {
@@ -57,13 +78,20 @@ function sendPostRequest(url, data) {
         },
         body: JSON.stringify(data)
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
     .then(data => {
         console.log('Success:', data);
         // Handle success response
+        // Ideally, inform the user of success in the UI
     })
     .catch((error) => {
         console.error('Error:', error);
         // Handle errors
+        // Ideally, display an error message to the user in the UI
     });
 }
