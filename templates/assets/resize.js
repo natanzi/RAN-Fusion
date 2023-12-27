364bb3bc81777dbfbc6f1fff6fc77b5ee540b2e5
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-27 16:17:14 -0500

    FYI

--- templates/assets/resize.js
+++ templates/assets/resize.js
@@ -1,97 +1,77 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Existing code for resizing...
+    // Resize Handlers
     var isResizing = false;
-
     var mapContainer = document.getElementById('map-container');
     var tabsContainer = document.getElementById('tabs-container');
-    var divider = document.getElementById('divider');
+    var divider = document.getElementById('divider');  // Assuming there's a divider element for resizing
 
     divider.addEventListener('mousedown', function(e) {
         isResizing = true;
     });
 
     document.addEventListener('mousemove', function(e) {
-        if (!isResizing) {
-            return;
-        }
-
+        if (!isResizing) return;
         var containerOffsetTop = mapContainer.offsetTop;
         var newHeight = e.clientY - containerOffsetTop;
 
         mapContainer.style.height = newHeight + 'px';
-        tabsContainer.style.height = 'calc(100vh - ' + newHeight + 'px - 30px)';
+        tabsContainer.style.height = 'calc(100vh - ' + newHeight + 'px)';
     });
 
-    document.addEventListener('mouseup', function(e) {
+    document.addEventListener('mouseup', function() {
         isResizing = false;
     });
 
-    // Add event listener for form submission
-    var form = document.getElementById('traffic-form'); // Ensure this ID matches your form
-    form.addEventListener('submit', function(e) {
-        e.preventDefault();
+    // Form Submission Handling
+    var forms = ['voiceTrafficForm', 'videoTrafficForm', 'gamingTrafficForm', 'iotTrafficForm', 'dataTrafficForm'];
+    forms.forEach(function(formId) {
+        var form = document.getElementById(formId);
+        if (form) {
+            form.addEventListener('submit', function(e) {
+                e.preventDefault();
+                var data = serializeForm(formId);
 
-        // Collect data from form
-        var data = {
-            bitrate_range: [
-                parseInt(document.getElementById('minBitrate').value, 10), 
-                parseInt(document.getElementById('maxBitrate').value, 10)
-            ],
-            jitter: parseFloat(document.getElementById('jitter').value),
-            delay: parseFloat(document.getElementById('delay').value),
-            packet_loss_rate: parseFloat(document.getElementById('packetLossRate').value)
-        };
-
-        // Validate data
-        if (validatePayload(data)) {
-            // Send POST request if validation passes
-            sendPostRequest('/update_gaming_traffic', data);
-        } else {
-            // Handle validation errors
-            console.error('Validation failed');
-            // Ideally, display an error message to the user in the UI
+                try {
+                    validatePayload(data);
+                    sendPostRequest('/update_' + formId.replace('TrafficForm', ''), data);
+                } catch (error) {
+                    console.error('Validation Error:', error);
+                    // Handle validation error (e.g., show error message to user)
+                }
+            });
         }
     });
 });
 
+function serializeForm(formId) {
+    var form = document.getElementById(formId);
+    var elements = form.elements;
+    var data = {};
+
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].type !== 'submit') {
+            data[elements[i].id] = elements[i].value;
+        }
+    }
+    return data;
+}
+
 function validatePayload(data) {
-    // Validation logic here...
-    return isValidType(data.bitrate_range, 'object') &&
-           Array.isArray(data.bitrate_range) &&
-           data.bitrate_range.length === 2 &&
-           isValidType(data.bitrate_range[0], 'number') &&
-           isValidType(data.bitrate_range[1], 'number') &&
-           isValidType(data.jitter, 'number') &&
-           isValidType(data.delay, 'number') &&
-           isValidType(data.packet_loss_rate, 'number');
+    // Implement specific validation logic here
+    // Throw an error if validation fails
+    // Example: if (!data.bitrate_range) throw new Error("Bitrate range is required.");
+    // Return nothing if validation passes
 }
 
-function isValidType(value, expectedType) {
-    return typeof value === expectedType;
-}
-
-function sendPostRequest(url, data) {
-    fetch(url, {
+function sendPostRequest(endpoint, data) {
+    fetch(endpoint, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-        // Handle success response
-        // Ideally, inform the user of success in the UI
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        // Handle errors
-        // Ideally, display an error message to the user in the UI
-    });
+    .then(response => response.json())
+    .then(data => console.log('Success:', data))
+    .catch(error => console.error('Error:', error));
 }
