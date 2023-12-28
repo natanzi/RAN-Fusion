804d7e7780cadb3633647f6d098bba837ff80854
Milad Natanzi <miladnatanzi@gmail.com>
2023-12-27 20:08:54 -0500

    FYI

--- templates/assets/formValidation.js
+++ templates/assets/formValidation.js
@@ -132,25 +132,29 @@ function handleFormSubmit(event) {
     }
 
     // Log the data to the console before sending it
-    console.log('Data being sent to the server:', data);
+console.log('Data being sent to the server:', data);
 
-    fetch(`http://127.0.0.1:5000/update_${formId.replace('Form', '')}_traffic`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-        alert('Traffic updated successfully');
-    })
+// Construct the correct endpoint based on the formId
+const service = formId.replace('Form', ''); // This should match the naming convention used in Flask routes
+const endpoint = `http://127.0.0.1:5000/update_${service}_traffic`;
+
+fetch(endpoint, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('Success:', data);
+    alert('Traffic updated successfully');
+})
     .catch((error) => {
         console.error('Error:', error);
         alert('Error updating traffic');
