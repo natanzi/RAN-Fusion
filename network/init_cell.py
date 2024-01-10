166fe034c5b58ff7859b7c34d40e17e4927a73fe
Milad Natanzi <miladnatanzi@gmail.com>
2024-01-10 12:03:01 -0500

    FYI

--- network/init_cell.py
+++ network/init_cell.py
@@ -14,11 +14,7 @@ def initialize_cells(gNodeBs, network_state):
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     config_dir = os.path.join(base_dir, 'Config_files')
     cells_config = load_json_config(os.path.join(config_dir, 'cell_config.json'))
-    network_state.add_cells(cells)  # Add the cells to the network state
     
-    # Initialize the DatabaseManager with the required parameters
-    db_manager = DatabaseManager(network_state)
-
     # Initialize Cells and link them to gNodeBs
     cells = []
     for cell_data in cells_config['cells']:
@@ -31,11 +27,16 @@ def initialize_cells(gNodeBs, network_state):
         cells.append(new_cell)
         network_state.cells[cell_id] = new_cell  # Add the new cell to the network state
 
+    # Now that cells list is populated, add it to the network state
+    network_state.add_cells(cells)
+    
+    # Initialize the DatabaseManager with the required parameters
+    db_manager = DatabaseManager(network_state)
+
     for cell in cells:
         # Serialize and write to InfluxDB
         point = cell.serialize_for_influxdb()
         db_manager.insert_data(point)
-
         # Link cells to gNodeBs
         gnodeb = gNodeBs.get(cell.gNodeB_ID)
         if gnodeb:
