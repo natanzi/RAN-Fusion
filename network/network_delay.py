eddc7b85d59b1ec6684890cfef17a9a27a9d85e1
Milad Natanzi <miladnatanzi@gmail.com>
2024-01-10 16:10:19 -0500

    FYI

--- network/network_delay.py
+++ network/network_delay.py
@@ -6,28 +6,6 @@ class NetworkDelay:
         # Additional attributes can be added here if needed
 
     def calculate_delay(self, cell_load):
-        # Existing logic to calculate network delay
+        # Logic to calculate network delay remains unchanged
         load_factor = 1 + cell_load / 100
         return self.base_delay * load_factor
-
-    # New method to determine if a handover is needed based on the network delay
-    def is_handover_required(self, network_delay, handover_threshold):
-        return network_delay > handover_threshold
-
-    # New method to select the target cell for handover
-    def select_target_cell(self, current_cell, all_cells):
-        # Placeholder logic to select a new cell with the least load
-        # This should be replaced with actual logic based on your network's requirements
-        return min(all_cells, key=lambda cell: cell.load)
-
-    # New method to perform the handover process
-    def perform_handover(self, ue, current_cell, all_cells, handover_threshold):
-        network_delay = self.calculate_delay(current_cell.load)
-        if self.is_handover_required(network_delay, handover_threshold):
-            target_cell = self.select_target_cell(current_cell, all_cells)
-            # Logic to perform the handover, e.g., update the UE's cell association
-            # This may involve interacting with other classes/methods in your codebase
-            # ...
-            ue.update_cell(target_cell)
-            return True, target_cell
-        return False, current_cell
