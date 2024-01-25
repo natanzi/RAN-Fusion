ad894055bf8bd97e30ca48d1712b9f25f9ef4557
Milad Natanzi <miladnatanzi@gmail.com>
2024-01-25 17:56:40 -0500

    FYI

--- network/initialize_network.py
+++ network/initialize_network.py
@@ -3,10 +3,10 @@
 from .init_gNodeB import initialize_gNodeBs
 from .init_cell import initialize_cells
 from .init_sector import initialize_sectors  # Import the initialize_sectors function
+from .init_ue import initialize_ues
 from logs.logger_config import ue_logger
 
 def initialize_network(num_ues_to_launch, gNodeBs_config, cells_config, sectors_config, ue_config, db_manager):
-    from network.init_ue import initialize_ues
 
     # Step 1: Instantiate gNodeBs
     gNodeBs = initialize_gNodeBs(gNodeBs_config, db_manager)
