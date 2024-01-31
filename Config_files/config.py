563136ddd360b219878dea8f8098d2c17979f42d
Milad Natanzi <miladnatanzi@gmail.com>
2024-01-31 11:38:24 -0500

    FYI

--- Config_files/config.py
+++ Config_files/config.py
@@ -1,6 +1,3 @@
-# This is the config.py in Config_files directory to load all JSON configs
-#Creating a Config class to encapsulate the different configuration dictionaries
-
 import os
 import json
 
@@ -11,22 +8,24 @@ class Config:
         self.cells_config = self.load_json_config('cell_config.json')
         self.sectors_config = self.load_json_config('sector_config.json')
         self.ue_config = self.load_json_config('ue_config.json')
-        self.network_map = self.load_or_generate_network_map()
+        # Load or generate the network map and assign it to the network_map_data attribute
+        self.network_map_data = self.load_or_generate_network_map()
 
     def load_json_config(self, filename):
         file_path = os.path.join(self.base_dir, 'Config_files', filename)
         with open(file_path, 'r') as file:
             return json.load(file)
-        
+
     def load_or_generate_network_map(self):
         network_map_path = os.path.join(self.base_dir, 'Config_files', 'network_map.json')
         if os.path.exists(network_map_path):
             with open(network_map_path, 'r') as file:
                 return json.load(file)
         else:
-            return self.generate_network_map()
-        
-    def network_map(self):
+            # If the network_map.json does not exist, generate and save it
+            return self.generate_and_save_network_map()
+
+    def generate_and_save_network_map(self):
         network_map = {
             "gNodeBs": []
         }
@@ -37,22 +36,18 @@ class Config:
             }
             associated_cells = [cell for cell in self.cells_config['cells'] if cell['gnodeb_id'] == gnodeb['gnodeb_id']]
             for cell in associated_cells:
-                # List sectors for this cell
                 sectors_for_cell = [{"sectorId": sector['sector_id']} 
                                     for sector in self.sectors_config['sectors'] 
                                     if sector['cell_id'] == cell['cell_id']]
-            
                 cell_entry = {
                     "cellId": cell['cell_id'],
                     "sectors": sectors_for_cell
                 }
                 gnodeb_entry["cells"].append(cell_entry)
             network_map["gNodeBs"].append(gnodeb_entry)
-    
+        
         # Save the network map to a JSON file in the Config_files directory
         output_file_path = os.path.join(self.base_dir, 'Config_files', 'network_map.json')
         with open(output_file_path, 'w') as file:
             json.dump(network_map, file, indent=2)
-
-
-
+        return network_map
