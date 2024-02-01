aa52cbbf7d8f518fbbeae4c1feca26814753bc5b
Milad Natanzi <miladnatanzi@gmail.com>
2024-02-01 14:50:48 -0500

    fyi

--- API Gateway/API.py
+++ API Gateway/API.py
@@ -1,12 +1,14 @@
 import sys
 import os
+# Append the project root directory to sys.path to ensure imports work correctly
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 from flask import Flask, request, jsonify
 from network.ue import UE
 from network.sector import Sector
 from threading import Lock
 import traceback
 import threading
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 app = Flask(__name__)
 lock = threading.Lock()
